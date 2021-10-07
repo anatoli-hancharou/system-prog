8bfc8bd626e00aba83b5fee3a4dac8bf4de03df5
Anatoli Hancharou <anatoli.han@mail.ru>
2021-10-07 11:53:02 +0300

    Add ninth lab and paint project implementation.

---
+++ lab_9/SP_lab9.cpp
@@ -0,0 +1,258 @@
+// SP_lab9.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "SP_lab9.h"
+#include <strsafe.h>
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int max = 1000;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_SPLAB9, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SPLAB9));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SPLAB9));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SPLAB9);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+void SearchInner(HWND hwnd, HKEY hKeyRoot, LPTSTR lpSubKey, TCHAR* s)
+{
+    if (--max <= 0) return;
+    LPTSTR lpEnd;
+    LONG lResult;
+    DWORD dwSize;
+    TCHAR szName[MAX_PATH];
+    HKEY hKey;
+    FILETIME ftWrite;
+
+    lResult = RegOpenKeyEx(hKeyRoot, lpSubKey, 0, KEY_READ, &hKey);
+    if (lResult != ERROR_SUCCESS)
+    {
+        return;
+    }
+
+    if (_tcsstr(lpSubKey, s) != NULL)
+        SendMessage(hwnd, LB_ADDSTRING, 0, (LPARAM)lpSubKey);
+
+    lpEnd = lpSubKey + lstrlen(lpSubKey);
+
+    if (*(lpEnd - 1) != TEXT('\\'))
+    {
+        *lpEnd = TEXT('\\');
+        lpEnd++;
+        *lpEnd = TEXT('\0');
+    }
+
+    dwSize = MAX_PATH;
+    lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
+        NULL, NULL, &ftWrite);
+
+    if (lResult == ERROR_SUCCESS)
+    {
+        int index = 0;
+        do {
+            index++;
+
+            StringCchCopy(lpEnd, MAX_PATH * 2, szName);
+
+            SearchInner(hwnd, hKeyRoot, lpSubKey, s);
+            if (max <= 0) return;
+
+            dwSize = MAX_PATH;
+
+            lResult = RegEnumKeyEx(hKey, index, szName, &dwSize, NULL,
+                NULL, NULL, &ftWrite);
+
+        } while (lResult == ERROR_SUCCESS);
+    }
+
+    lpEnd--;
+    *lpEnd = TEXT('\0');
+
+    RegCloseKey(hKey);
+}
+
+void Search(HWND hwnd, TCHAR* s) {
+    SendMessage(hwnd, LB_RESETCONTENT, 0, 0);
+
+    LPTSTR lpSubKey = (LPTSTR)L"Software";
+    TCHAR szSearchKey[MAX_PATH * 2];
+    StringCchCopy(szSearchKey, MAX_PATH * 2, lpSubKey);
+
+    max = 1000;
+    SearchInner(hwnd, HKEY_LOCAL_MACHINE, szSearchKey, s);
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    static HWND edit;
+    static HWND button;
+    static HWND list;
+
+
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        edit = CreateWindowEx(WS_EX_CLIENTEDGE, L"edit", L"", WS_TABSTOP | WS_CHILD | WS_VISIBLE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+            50, 650, 850, 30, hWnd, (HMENU)101, hInst, NULL);
+
+        button = CreateWindow(L"button", L"Search", WS_TABSTOP | WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+            900, 650, 150, 30, hWnd, (HMENU)102, hInst, NULL);
+
+        list = CreateWindowEx(WS_EX_CLIENTEDGE, L"listbox", L"", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_AUTOVSCROLL,
+            50, 50, 1000, 600, hWnd, (HMENU)103, hInst, NULL);
+    }
+    break;
+    case WM_COMMAND:
+        // Parse the menu selections:
+        switch (LOWORD(wParam))
+        {
+        case 102:
+        {
+            int len;
+            len = SendMessage(edit, WM_GETTEXTLENGTH, 0, 0);
+            TCHAR buffer[256];
+            SendMessage(edit, WM_GETTEXT, len + 1, (LPARAM)buffer);
+            Search(list, buffer);
+        }
+        break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        // TODO: Add any drawing code here...
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ pnt/pnt/pnt.cpp
@@ -0,0 +1,407 @@
+﻿// pnt.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "pnt.h"
+
+#define MAX_LOADSTRING 100
+#define IDM_CLEARBUTTON 10001
+#define IDM_RUBBERBUTTON 5
+#define ID_LIST 101
+#define ID_COLORS 6
+#define ID_WIDTH 7
+#define ID_BRUSHCOLOR 8
+
+wchar_t listBoxStr[15];
+HWND hListBox;
+HWND ColorBox;
+HWND WidthBox;
+HWND BrushColorBox;
+
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+BOOL Line(HDC hdc, int x1, int y1, int x2, int y2);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_PNT, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PNT));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PNT));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PNT);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+HPEN hPen = NULL;
+HPEN pen = NULL;
+HBRUSH hb = NULL;
+
+BOOL fDraw = FALSE;
+POINT ptPrevious = { 0 };
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HDC hdc;
+    static int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+    static bool aa = true;
+    static int uSelectedItem = 0;
+    static int colorIndex = 0;
+    static int brushIndex = 4;
+    static int width = 1;
+    static int colors[5][3] = {{0, 0 ,0},
+                               {255, 0, 0},
+                               {0, 255, 0},
+                               {0, 0, 255},
+                               {255,255,255}};
+
+    switch (message)
+    {
+    case WM_CREATE:
+        {
+            hPen = CreatePen(PS_SOLID, 25, RGB(255,255,255));
+
+            hListBox = CreateWindow(L"listbox", NULL,
+            WS_CHILD | WS_VISIBLE | LBS_STANDARD |
+            LBS_WANTKEYBOARDINPUT,
+            201, 1, 100, 40,
+            hWnd, (HMENU)ID_LIST, hInst, NULL);
+            SendMessage(hListBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Line");
+            SendMessage(hListBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Circle");
+            SendMessage(hListBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Rectangle");
+
+            ColorBox = CreateWindow(L"listbox", NULL,
+                WS_CHILD | WS_VISIBLE | LBS_NOTIFY |
+                LBS_WANTKEYBOARDINPUT | WS_VSCROLL | WS_BORDER,
+                301, 1, 100, 40,
+                hWnd, (HMENU)ID_COLORS, hInst, NULL);
+            SendMessage(ColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Black");
+            SendMessage(ColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Red");
+            SendMessage(ColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Green");
+            SendMessage(ColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Blue");
+
+            WidthBox = CreateWindow(L"listbox", NULL,
+                WS_CHILD | WS_VISIBLE | LBS_NOTIFY |
+                LBS_WANTKEYBOARDINPUT | WS_VSCROLL | WS_BORDER,
+                401, 1, 100, 40,
+                hWnd, (HMENU)ID_WIDTH, hInst, NULL);
+            SendMessage(WidthBox, LB_ADDSTRING, 0,
+                (LPARAM)L"1px");
+            SendMessage(WidthBox, LB_ADDSTRING, 0,
+                (LPARAM)L"5px");
+            SendMessage(WidthBox, LB_ADDSTRING, 0,
+                (LPARAM)L"10px");
+            SendMessage(WidthBox, LB_ADDSTRING, 0,
+                (LPARAM)L"15px");
+
+            BrushColorBox = CreateWindow(L"listbox", NULL,
+                WS_CHILD | WS_VISIBLE | LBS_NOTIFY |
+                LBS_WANTKEYBOARDINPUT | WS_VSCROLL | WS_BORDER,
+                501, 1, 100, 40,
+                hWnd, (HMENU)ID_BRUSHCOLOR, hInst, NULL);
+            SendMessage(BrushColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Black brush");
+            SendMessage(BrushColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Red brush");
+            SendMessage(BrushColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Green brush");
+            SendMessage(BrushColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"Blue brush");
+            SendMessage(BrushColorBox, LB_ADDSTRING, 0,
+                (LPARAM)L"None");
+
+            SendMessage(WidthBox, LB_SETCURSEL, 0, 0);
+            SendMessage(BrushColorBox, LB_SETCURSEL, 4, 0);
+            SendMessage(ColorBox, LB_SETCURSEL, 0, 0);
+            SendMessage(hListBox, LB_SETCURSEL, 0, 0);
+
+            HWND hButtonClearWnd = CreateWindow(_T("BUTTON"), _T("Clear"),
+            WS_CHILD | WS_VISIBLE, 1, 1, 100, 30, hWnd,
+            (HMENU)IDM_CLEARBUTTON, NULL, NULL);
+
+            HWND hButtonRubberWnd = CreateWindow(_T("BUTTON"), _T("Rubber"),
+                WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 101, 1, 100, 30, hWnd,
+                (HMENU)IDM_RUBBERBUTTON, NULL, NULL);
+
+            break;
+        }
+    case WM_LBUTTONDOWN:
+        {
+            if (uSelectedItem == -1)
+            {
+                fDraw = TRUE;
+                ptPrevious.x = LOWORD(lParam);
+                ptPrevious.y = HIWORD(lParam);               
+            }
+
+            x1 = LOWORD(lParam);
+            y1 = HIWORD(lParam);
+            break;
+        }
+    case WM_MOUSEMOVE:
+        {
+            if (fDraw && uSelectedItem == -1)
+            {
+                hdc = GetDC(hWnd);
+                SelectObject(hdc, hPen);
+                MoveToEx(hdc, ptPrevious.x, ptPrevious.y, NULL);
+                LineTo
+                (
+                    hdc,
+                    ptPrevious.x = LOWORD(lParam),
+                    ptPrevious.y = HIWORD(lParam)
+                );
+                ReleaseDC(hWnd, hdc);
+            }
+            break;
+        }
+    case WM_LBUTTONUP:
+        {
+            if (fDraw && uSelectedItem == -1)
+            {
+                hdc = GetDC(hWnd);
+                MoveToEx(hdc, ptPrevious.x, ptPrevious.y, NULL);
+                LineTo(hdc, LOWORD(lParam), HIWORD(lParam));
+                ReleaseDC(hWnd, hdc);
+                fDraw = FALSE;
+            }
+            x2 = LOWORD(lParam);
+            y2 = HIWORD(lParam);
+            hdc = GetDC(hWnd);
+
+            auto colorPen = colors[colorIndex];
+            pen = CreatePen(PS_SOLID, width, RGB(colorPen[0], colorPen[1], colorPen[2]));
+            auto colorBrush = colors[brushIndex];
+            hb = CreateSolidBrush(RGB(colorBrush[0], colorBrush[1], colorBrush[2]));
+
+            SelectObject(hdc, pen);
+            SelectObject(hdc, hb);
+
+            if (uSelectedItem == 0) {
+                Ellipse(hdc, x1, y1, x2, y2);
+            }
+            else if (uSelectedItem == 1) {
+                Line(hdc, x1, y1, x2, y2);
+            }
+            else if (uSelectedItem == 2) {
+                Rectangle(hdc, x1, y1, x2, y2);
+            }
+         //   ReleaseDC(hWnd, hdc);
+            break;
+        }
+    case WM_COMMAND:
+        {
+            BOOL rubberChecked = IsDlgButtonChecked(hWnd, IDM_RUBBERBUTTON);
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case ID_LIST:
+                CheckDlgButton(hWnd, IDM_RUBBERBUTTON, BST_UNCHECKED);
+                // Если пользователь сделал двойной щелчок
+                if (HIWORD(wParam) == LBN_DBLCLK || HIWORD(wParam) == LBN_SELCHANGE)
+                {
+                    uSelectedItem = (int)SendMessage(hListBox,
+                        LB_GETCURSEL, 0, 0);
+                }
+                break;
+            case ID_COLORS:
+                if (HIWORD(wParam) == LBN_DBLCLK || HIWORD(wParam) == LBN_SELCHANGE)
+                {
+                    colorIndex = (int)SendMessage(ColorBox,
+                        LB_GETCURSEL, 0, 0);
+                }
+                break;
+            case ID_BRUSHCOLOR:
+                if (HIWORD(wParam) == LBN_DBLCLK || HIWORD(wParam) == LBN_SELCHANGE)
+                {
+                    brushIndex = (int)SendMessage(BrushColorBox,
+                        LB_GETCURSEL, 0, 0);
+                }
+                break;
+            case ID_WIDTH:
+                if (HIWORD(wParam) == LBN_DBLCLK || HIWORD(wParam) == LBN_SELCHANGE)
+                {
+                    int index = (int)SendMessage(WidthBox,
+                        LB_GETCURSEL, 0, 0);
+                    switch (index)
+                    {
+                    case 0:
+                        width = 1;
+                        break;
+                    case 1:
+                        width = 5;
+                        break;
+                    case 2:
+                        width = 10;
+                        break;
+                    case 3:
+                        width = 15;
+                        break;
+                    default:
+                        width = 1;
+                        break;
+                    }
+                }
+                break;
+            case IDM_CLEARBUTTON:
+                RECT r;
+                GetClientRect(hWnd, &r);
+                InvalidateRect(hWnd, &r, TRUE);
+                UpdateWindow(hWnd);
+                break;
+            case IDM_RUBBERBUTTON:
+                uSelectedItem = -1;
+                break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+BOOL Line(HDC hdc, int x1, int y1, int x2, int y2)
+{
+    MoveToEx(hdc, x1, y1, NULL); //сделать текущими координаты x1, y1
+    return LineTo(hdc, x2, y2);
+}
